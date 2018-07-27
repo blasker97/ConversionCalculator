c2c4a78dedd93e4deacc2ad0d64a714ca81f0f0b
Brian Lasker <brianlasker@Brians-MacBook-Pro.local>
2018-07-27 12:06:25 -0500

    UPDATED: Implemented calculator functionality

--- Conversion Calculator/Convert.swift
+++ Conversion Calculator/Convert.swift
@@ -9,7 +9,75 @@
 import UIKit
 
 struct Convert {
-    let label : String
-    let input : String
-    let output : String
+    var label : String
+    var inputUnit : String
+    var outputUnit : String
+    var inputNum : Double?
+    var outputNum : Double?
+    var isDecimal : Bool
+    var decimalPlace : Int
+    
+    mutating func addNumber (_ number : Double) {
+        if inputNum != nil {
+            
+            var tempInput = inputNum!
+            
+            if isDecimal == false {
+                if tempInput >= 0 {
+                    tempInput = tempInput * 10 + number
+                    inputNum? = tempInput
+                }
+                
+                if tempInput < 0 {
+                    tempInput = tempInput * 10 - number
+                    inputNum? = tempInput
+                }
+            } else {
+                if tempInput >= 0 {
+                    tempInput = tempInput + number / pow(10,Double(decimalPlace))
+                    inputNum? = tempInput
+                }
+                
+                if tempInput < 0 {
+                    tempInput = tempInput - number / pow(10,Double(decimalPlace))
+                    inputNum? = tempInput
+                }
+            }
+        } else {
+            inputNum = number
+        }
+    }
+    
+    mutating func clear() {
+        inputNum = nil
+        outputNum = nil
+        isDecimal = false
+        decimalPlace = 1
+    }
+    
+    mutating func flipSign() {
+        if inputNum != nil && inputNum != 0 {
+            inputNum = inputNum! * (-1)
+        }
+        if isDecimal == true {
+            decimalPlace -= 1
+        }
+    }
+    
+    mutating func convert() {
+        switch self.label {
+        case "fahrenheit to celsius":
+            outputNum = (inputNum! - 32) * 5/9
+        case "celsius to fahrenheit":
+            outputNum = inputNum! * (9/5) + 32
+        case "miles to kilometers":
+            outputNum = inputNum! * 1.609344
+        case "kilometers to miles":
+            outputNum = inputNum! / 1.609344
+        default:
+            break
+        }
+    }
+    
 }
+
--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -9,48 +9,142 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
-   
-    var conversionOptions = [Convert(label : "fahrenheit to celcius", input : "°F", output : "°C"),
-                             Convert(label : "celcius to fahrenheit", input : "°C", output : "°F"),
-                             Convert(label : "miles to kilometers", input : "mi", output : "km"),
-                             Convert(label : "kilometers to miles", input : "km", output : "mi")]
+    
+    var currentConversion = Convert(label : "fahrenheit to celsius", inputUnit : "°F", outputUnit : "°C", inputNum : nil, outputNum : nil, isDecimal : false, decimalPlace : 1)
+    
     
     @IBOutlet weak var inputDisplay: UITextField!
     @IBOutlet weak var outputDisplay: UITextField!
     
+    @IBAction func signButton(_ sender: Any) {
+        currentConversion.flipSign()
+        update()
+    }
+    @IBAction func clearButton(_ sender: Any) {
+        currentConversion.clear()
+        update()
+    }
+    @IBAction func decimalButton(_ sender: Any) {
+        if currentConversion.inputNum != nil && currentConversion.isDecimal != true {
+            currentConversion.isDecimal = true
+            self.inputDisplay.text = "\(currentConversion.inputNum!.cleanValue). \(currentConversion.inputUnit)"
+        }
+    }
+    @IBAction func zeroButton(_ sender: Any) {
+        currentConversion.addNumber(0)
+        update()
+    }
+    @IBAction func oneButton(_ sender: Any) {
+        currentConversion.addNumber(1)
+        update()
+    }
+    @IBAction func twoButton(_ sender: Any) {
+        currentConversion.addNumber(2)
+        update()
+    }
+    @IBAction func threeButton(_ sender: Any) {
+        currentConversion.addNumber(3)
+        update()
+    }
+    @IBAction func fourButton(_ sender: Any) {
+        currentConversion.addNumber(4)
+        update()
+    }
+    @IBAction func fiveButton(_ sender: Any) {
+        currentConversion.addNumber(5)
+        update()
+    }
+    @IBAction func sixButton(_ sender: Any) {
+        currentConversion.addNumber(6)
+        update()
+    }
+    @IBAction func sevenButton(_ sender: Any) {
+        currentConversion.addNumber(7)
+        update()
+    }
+    @IBAction func eightButton(_ sender: Any) {
+        currentConversion.addNumber(8)
+        update()
+    }
+    @IBAction func nineButton(_ sender: Any) {
+        currentConversion.addNumber(9)
+        update()
+    }
+    
+    func update() {
+        let inputFormatter = NumberFormatter()
+        inputFormatter.minimumFractionDigits = currentConversion.decimalPlace
+        
+        let outputFormatter = NumberFormatter()
+        outputFormatter.maximumFractionDigits = 4
+
+        if let inputNumber = currentConversion.inputNum {
+            currentConversion.convert()
+            if currentConversion.isDecimal == true {
+                self.inputDisplay.text = "\(inputFormatter.string(for: inputNumber) ?? "") \(currentConversion.inputUnit)"
+                self.outputDisplay.text = "\(outputFormatter.string(for: currentConversion.outputNum!) ?? "") \(currentConversion.outputUnit)"
+                currentConversion.decimalPlace += 1
+            } else {
+                self.inputDisplay.text = "\(inputNumber.cleanValue) \(currentConversion.inputUnit)"
+                self.outputDisplay.text = "\(outputFormatter.string(for: currentConversion.outputNum!) ?? "") \(currentConversion.outputUnit)"
+            }
+        } else {
+            self.inputDisplay.text = "\(currentConversion.inputUnit)"
+            self.outputDisplay.text = "\(currentConversion.outputUnit)"
+        }
+    }
+    
     @IBAction func convertButton(_ sender: Any) {
         let alert = UIAlertController(title: "", message: "Choose Converter", preferredStyle: UIAlertControllerStyle.actionSheet)
-        alert.addAction(UIAlertAction(title: conversionOptions[0].label, style: UIAlertActionStyle.default, handler: {
+        alert.addAction(UIAlertAction(title: "fahrenheit to celsius", style: UIAlertActionStyle.default, handler: {
             (alertAction) -> Void in
-            self.inputDisplay.text = self.conversionOptions[0].input
-            self.outputDisplay.text = self.conversionOptions[0].output
+            self.currentConversion.label = "fahrenheit to celsius"
+            self.currentConversion.inputUnit = "°F"
+            self.currentConversion.outputUnit = "°C"
+            if self.currentConversion.isDecimal == true {
+                self.currentConversion.decimalPlace -= 1
+            }
+            self.update()
         }))
-        alert.addAction(UIAlertAction(title: conversionOptions[1].label, style: UIAlertActionStyle.default, handler: {
+        alert.addAction(UIAlertAction(title: "celsius to fahrenheit", style: UIAlertActionStyle.default, handler: {
             (alertAction) -> Void in
-            self.inputDisplay.text = self.conversionOptions[1].input
-            self.outputDisplay.text = self.conversionOptions[1].output
+            self.currentConversion.label = "celsius to fahrenheit"
+            self.currentConversion.inputUnit = "°C"
+            self.currentConversion.outputUnit = "°F"
+            if self.currentConversion.isDecimal == true {
+                self.currentConversion.decimalPlace -= 1
+            }
+            self.update()
         }))
-        alert.addAction(UIAlertAction(title: conversionOptions[2].label, style: UIAlertActionStyle.default, handler: {
+        alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertActionStyle.default, handler: {
             (alertAction) -> Void in
-            self.inputDisplay.text = self.conversionOptions[2].input
-            self.outputDisplay.text = self.conversionOptions[2].output
+            self.currentConversion.label = "miles to kilometers"
+            self.currentConversion.inputUnit = "mi"
+            self.currentConversion.outputUnit = "km"
+            if self.currentConversion.isDecimal == true {
+                self.currentConversion.decimalPlace -= 1
+            }
+            self.update()
         }))
-        alert.addAction(UIAlertAction(title: conversionOptions[3].label, style: UIAlertActionStyle.default, handler: {
+        alert.addAction(UIAlertAction(title: "kilometers to miles", style: UIAlertActionStyle.default, handler: {
             (alertAction) -> Void in
-            self.inputDisplay.text = self.conversionOptions[3].input
-            self.outputDisplay.text = self.conversionOptions[3].output
+            self.currentConversion.label = "kilometers to miles"
+            self.currentConversion.inputUnit = "km"
+            self.currentConversion.outputUnit = "mi"
+            if self.currentConversion.isDecimal == true {
+                self.currentConversion.decimalPlace -= 1
+            }
+            self.update()
         }))
         
         self.present(alert, animated: true, completion: nil)
-        
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        inputDisplay.text = conversionOptions[0].input
-        outputDisplay.text = conversionOptions[0].output
+        update()
         // Do any additional setup after loading the view.
     }
 
@@ -58,8 +152,6 @@ class ConverterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
     
 
     /*
@@ -72,4 +164,10 @@ class ConverterViewController: UIViewController {
     }
     */
 
+}
+
+extension Double {
+    var cleanValue: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0lf", self) : String(self)
+    }
 }
